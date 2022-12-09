868e24c348d96a37bb6b415ff903163a9b09eb6a
kasarama <cph-mw216@cphbusiness.dk>
2022-12-09 10:42:36 +0100

    edit toString

--- src/main/java/com/teamrocket/model/camunda/DeliveryTaskValue.java
+++ src/main/java/com/teamrocket/model/camunda/DeliveryTaskValue.java
@@ -15,4 +15,15 @@ public class DeliveryTaskValue {
     int restaurantAddressId;
     String areaId;
     long pickupTime;
+
+
+    public String toJsonString() {
+        return "{" +
+                "orderId=" + orderId +
+                ", restaurantName='" + restaurantName + '\'' +
+                ", restaurantAddressId=" + restaurantAddressId +
+                ", areaId='" + areaId + '\'' +
+                ", pickupTime=" + pickupTime +
+                '}';
+    }
 }
